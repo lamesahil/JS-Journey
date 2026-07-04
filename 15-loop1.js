3f6fe59704b40038d60d6693b55ef9c54bb2d1a9
lamesahil <iamsahilt20@gmail.com>
2026-07-04 19:22:34 +0530

    ForLoopBreakContinue

--- 15-loop1.js
+++ 15-loop1.js
@@ -1,7 +1,49 @@
-const myNums = [1, 2, 3, 4];
+// for
 
-const result = myNums.map( (num) => {
-    return num + 10;
-} );
+for (let i = 10; i <= 20; i++) {
+    const element = i;
+    if (element == 19) {
+        //console.log("19 is best number");
+    }
+    //console.log(element);
+    
+}
 
-console.log(result);
+// console.log(element); // ReferenceError: element is not defined
+
+for (let i = 2; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+let myDC = ["flash", "batman", "superman"]
+//console.log(myDC.length);
+for (let index = 0; index < myDC.length; index++) {
+    const element = myDC[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+// for (let index = 10; index <= 20; index++) {
+//     if (index == 19) {
+//         console.log(`Detected 19`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 10; index <= 20; index++) {
+    if (index == 19) {
+        console.log(`Detected 19`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
